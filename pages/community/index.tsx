3ea9891ca355c1c073c69371732f02462a318280
padd60 <padd60@naver.com>
2022-11-22 02:26:32 +0900

    [fix] UI 컴포넌트화 및 리펙토링

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,24 +1,30 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
+import FloatingButton from '../../components/floating-button';
 import Layout from '../../components/layout';
 
 const Community: NextPage = () => {
   return (
-    <Layout title={'동네생활'} hasTabBar>
-      <div className="py-16 px-4 space-y-8">
+    <Layout hasTabBar title="동네생활">
+      <div className="space-y-4 divide-y-[2px]">
         {[1, 2, 3, 4, 5, 6].map((_, i) => (
-          <div key={i} className="flex cursor-pointer flex-col items-start">
-            <span className="flex items-center px-2.5 py-0.5 rounded-full text-xs bg-gray-100 font-medium text-gray-800">
+          <Link
+            key={i}
+            href={`/community/${i}`}
+            className="flex cursor-pointer flex-col pt-4 items-start"
+          >
+            <span className="flex ml-4 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
               동네질문
             </span>
-            <div className="mt-2 text-gray-700">
+            <div className="mt-2 px-4 text-gray-700">
               <span className="text-orange-500 font-medium">Q.</span> What is
               the best mandu restaurant?
             </div>
-            <div className="mt-5 flex items-center justify-between w-full text-gray-500 font-medium text-xs">
+            <div className="mt-5 px-4 flex items-center justify-between w-full text-gray-500 font-medium text-xs">
               <span>니꼬</span>
               <span>18시간 전</span>
             </div>
-            <div className="flex space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px] w-full">
+            <div className="flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t   w-full">
               <span className="flex space-x-2 items-center text-sm">
                 <svg
                   className="w-4 h-4"
@@ -54,9 +60,9 @@ const Community: NextPage = () => {
                 <span>답변 1</span>
               </span>
             </div>
-          </div>
+          </Link>
         ))}
-        <button className="fixed hover:bg-orange-500 transition-colors cursor-pointer bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-4 text-white">
+        <FloatingButton href="/community/write">
           <svg
             className="w-6 h-6"
             fill="none"
@@ -71,7 +77,7 @@ const Community: NextPage = () => {
               d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
             ></path>
           </svg>
-        </button>
+        </FloatingButton>
       </div>
     </Layout>
   );
