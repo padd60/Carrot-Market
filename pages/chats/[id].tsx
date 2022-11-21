8acdaaa72f237f44b0257f95a8e9945d8dc10b25
padd60 <padd60@naver.com>
2022-11-21 23:10:55 +0900

    [style] 채팅 상세 페이지 UI 스타일 추가

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -2,24 +2,35 @@ import type { NextPage } from 'next';
 
 const ChatDetail: NextPage = () => {
   return (
-    <div>
-      <div>
-        <div />
-        <div>Hi how much are you selling them for?</div>
+    <div className="py-10 px-4 space-y-4">
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-400" />
+        <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+          Hi how much are you selling them for?
+        </div>
       </div>
-      <div>
-        <div />
-        <div>I want ￦20,000</div>
+      <div className="flex flex-row-reverse items-center space-x-2 space-x-reverse">
+        <div className="w-8 h-8 rounded-full bg-slate-400" />
+        <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+          I want ￦20,000
+        </div>
       </div>
-      <div>
-        <div />
-        <div>미쳤어</div>
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-400" />
+        <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+          미쳤어
+        </div>
       </div>
-      <div>
-        <div>
-          <input type="text" />
-          <div>
-            <span>&rarr;</span>
+      <div className="fixed w-full mx-auto max-w-md bottom-2 inset-x-0">
+        <div className="flex relative items-center">
+          <input
+            type="text"
+            className="shadow-sm rounded-full w-full border-gray-300 pr-12 focus:ring-orange-500 focus:outline-none focus:border-orange-500"
+          />
+          <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+            <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center hover:bg-orange-600 bg-orange-500 rounded-full px-3 text-sm text-white">
+              &rarr;
+            </button>
           </div>
         </div>
       </div>
