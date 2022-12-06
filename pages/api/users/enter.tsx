fe2da17256d4cd6ae5311ce9c31b8b288420ed08
padd60 <padd60@naver.com>
2022-12-06 22:40:42 +0900

    [feat] 계정 생성 로직 구현

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler from '@libs/server/withHandler';
+import client from '@libs/client/client';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: Number(phone) } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: 'Anonymous',
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
   return res.status(200).end();
 }
 
